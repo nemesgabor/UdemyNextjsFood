6a7fec875dade037e45c5f7ecc241a8fd2545dc1
Nemesg <nemesg@collabit.hu>
2024-02-27 13:40:55 +0100

    122

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,9 @@
 import ImagePicker from '@/app/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-    async function shareMeal() {
-        'use server';
-        
-    }
+  
   return (
     <>
       <header className={classes.header}>
@@ -15,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} >
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,7 +41,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name='image' />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
